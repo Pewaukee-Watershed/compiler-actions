c0f7945a9801b3767e1ea12d7aa10cd4158a15d3
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-12-11 10:03:48 -0600

    Use then instead of await

--- index.js
+++ index.js
@@ -28,15 +28,16 @@ console.time('transform');
   const files = await globber.glob()
   
   const renderFile = `
-const { default: App } = await import(window.location.pathname
+import(window.location.pathname
   .split('?')[1]
   .split('&')
   .map(p => p.split('='))
   .find(([p]) => p === 'component')[1]
-)
-const app = React.createElement(App)
-const appDiv = document.getElementById('app')
-ReactDOM.render(app, appDiv)
+).then(({ default: App }) => {
+  const app = React.createElement(App)
+  const appDiv = document.getElementById('app')
+  ReactDOM.render(app, appDiv)
+})
 `
   const renderBlob = await createBlob(renderFile)
   
